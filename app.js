fd960c379d785dca8a4046dc814bb21767576db2
Qu4k3 <paul.guillamon@gmail.com>
2019-02-08 17:46:07 +0100

    g2i7

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 // Add headers
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
