10e4fb694bfa07f944e04d88c3f333126d4d263f
Qu4k3 <paul.guillamon@gmail.com>
2019-04-12 14:50:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,9 +19,14 @@ app.use(bodyParser.json());
 // CORS settings
 
 // Add headers
-app.use(function (req, res, next) {
+app.use(function (req, res, next) {  
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500, http://127.0.0.1:3000');
+  var allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3000'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET'); // GET, POST, OPTIONS, PUT, PATCH, DELETE
   // Request headers you wish to allow
