ca4dba7c6c24eb0975704c82aec7f07bbf694ae7
Qu4k3 <paul.guillamon@gmail.com>
2020-04-15 20:58:13 +0200

    New model structure

--- models/war.js
+++ models/war.js
@@ -5,7 +5,7 @@ const generate = require('nanoid/generate');
 const config = require('./../config.json');
 var Schema = mongoose.Schema;
 
-mongoose.set('useCreateIndex', true)
+mongoose.set('useCreateIndex', true);
 
 var WarSchema = Schema({
     war_id: {
@@ -14,11 +14,22 @@ var WarSchema = Schema({
         unique: true
     },
     played_at: Date,
-    game: {name: String, mode: String},
+    game: {
+        name: String,
+        mode: String
+    },
     type: String,
     tags: Array,
-    home_team: {team: String, score: Number, penality: Number, players: [{player: String, score: Number}]},
-    away_team: {team: String, score: Number, penality: Number, players: [{player: String, score: Number}]}
+    results: [{
+        team: String,
+        host: Boolean,
+        score: Number,
+        penality: Number,
+        players: [{
+            player: String,
+            score: Number
+        }]
+    }]
 });
 
 module.exports = mongoose.model('War', WarSchema);
